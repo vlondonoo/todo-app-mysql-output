35859b689af83a8f10be1b50dd1a6043d4674403
vlondonoo <vlondonoo@gmail.com>
2022-01-30 01:19:40 -0500

    feat: postgres configuration

--- db/sequelize.js
+++ db/sequelize.js
@@ -4,8 +4,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
-const sequelize = new Sequelize(process.env.MYSQLDB_DATABASE, process.env.MYSQLDB_USER, process.env.MYSQLDB_ROOT_PASSWORD, {
-  host: process.env.HOST,
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: process.env.DIALECT,
   pool: {
     max: 10,
